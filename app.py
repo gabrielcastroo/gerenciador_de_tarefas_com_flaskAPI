796d04e5037a700e8a44ca0a2bd86eddb9e0d618
Gabriel Castro <gabrielcastrog2@gmail.com>
2022-01-29 21:22:39 -0300

    config: initialize SQL Alchemy to configure the DataBase

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///test.db'
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 
